15ea19c5ab0faa454e5b64021b3a8c23c7faf30e
inderjeet547 <hazaninderjeetsingh@gmail.com>
2020-12-15 11:45:52 +0530

    Committed Changes

---
+++ Roll-Dice-Game-App/Roll-Dice-Game-App/Model/Logic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roll_Dice_Game_App.Model
+{
+    class Logic
+    {
+        int total = 0, count = 1;
+        char input;
+        public void RunOrHold()
+        {
+            System.Random random = new System.Random();
+            int randomnum = random.Next(1, 6);
+
+            Console.WriteLine(randomnum);
+            total += randomnum;
+
+            if (randomnum == 1)
+            {
+                if(total >= 20)
+                {
+                    Console.WriteLine("Turn Over...!" + "\nTotal Score : " + total);
+                }
+            }
+            else if(randomnum == 1 && total < 20)
+            {
+                total = 0;
+                Console.WriteLine("Score Reset to : " + total);
+                Play();
+            }
+            else
+            {
+                Play();
+            }
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Roll or Hold...(R/H) ? ");
+            input = Convert.ToChar(Console.ReadLine().ToLower());
+            if (input == 'r' && total < 20)
+            {
+                RunOrHold();
+            }
+            else if (input == 'h' && total < 20)
+            {
+                count++;
+                Console.WriteLine("Turn : " + count);
+                RunOrHold();
+            }
+            else
+            {
+                Console.WriteLine("Turn Over...!" + "\nTotal Score : " + total);
+                return;
+            }
+        }
+    }
+}
---
+++ Roll-Dice-Game-App/Roll-Dice-Game-App/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Roll-Dice-Game-App")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Roll-Dice-Game-App")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6dd8301f-2103-416e-86dc-1a5603aafcd3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
